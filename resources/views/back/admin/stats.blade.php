7c7cbf64a46b472bf431a9dbcc8ea1193a5b052e
ibraum <ibraumodnok@gmail.com>
2025-05-03 10:29:06 +0000

    admin dashboard

--- resources/views/back/admin/stats.blade.php
+++ resources/views/back/admin/stats.blade.php
@@ -6,22 +6,20 @@
     @endphp
     {{-- {{ dd($stats) }} --}}
     <div class="flex gap-3">
-        <div class="w-[calc(100%-250px)] border rounded shadow px-2 py-2 flex gap-2 flex-wrap">
-            <div class="flex flew-wrap w-full gap-2">
-                <div class="border w-[100px] h-[200px] rounded flex items-center justify-center text-xl text-slate-600 bg-blue-500/10 relative overflow-hidden grow">
+        <div class="w-[calc(100%-250px)] border rounded shadow px-2 py-2 flex gap-2 flex-wrap grow">
+            <div class="flex flex-wrap w-full gap-2">
+                <div class="border w-[200px] h-[250px] rounded flex items-center justify-center text-xl text-slate-600 bg-blue-500/10 relative overflow-hidden grow">
                     <div class="text-2xl">{{ $ca }} f cfa</div>
                     <div class="absolute top-0 left-1 flex gap-3"><span><i class="fi fi-rr-chart-mixed-up-circle-dollar"></i></span> Chiffre d'affaire total</div>
                     <img src="{{ asset('/images/dash-bubble.svg') }}" alt="" class="absolute bottom-[-35px] right-[-25px]">
                 </div>
-                <div id="chart1" class="border w-[300px] h-[200px] rounded flex items-center justify-center text-xl text-slate-600 relative overflow-hidden grow">
-                    <div class="absolute top-0 left-1 flex gap-3"><span><i class="fi fi-rr-chart-mixed-up-circle-dollar"></i></span> Chiffre d'affaire total</div>
-                    {{-- <img src="{{ asset('/images/dash-bubble.svg') }}" alt="" class="absolute bottom-[-35px] right-[-25px]"> --}}
+                <div id="chart1" class="border w-[300px] h-[250px] rounded flex items-center justify-center text-xl text-slate-600 relative overflow-hidden grow">
                 </div>
             </div>
         </div>
-        <div class="w-[250px] border flex p-2 rounded flex flex-wrap gap-3 shadow">
+        <div class="w-[250px] border flex p-2 rounded flex flex-wrap gap-3 shadow grow">
             @foreach ($stats as $key => $stat)
-                <div class="{{ $stat[1] }} w-full h-[100px] border grow rounded flex items-center justify-start gap-3 pl-3">
+                <div class="{{ $stat[1] }} w-full h-[90px] border grow rounded flex items-center justify-start gap-3 pl-3">
                     <div class="text-lg h-[50px] w-[50px] rounded-full flex items-center justify-center {{ $stat[3] }} {{ $stat[1] }}"><i class="{{ $stat[2] }}"></i></div> {{ count($stat[0]) }} - {{ $key }}
                 </div>
             @endforeach
@@ -42,11 +40,11 @@
       series: [
         {
           name: 'Nombre de commande - ' + new Date().getFullYear(),
-          data: [],
+          data: @json($totaux),
         },
       ],
       chart: {
-        height: 300,
+        height: 200,
         type: 'line',
         toolbar: {
           show: false,
