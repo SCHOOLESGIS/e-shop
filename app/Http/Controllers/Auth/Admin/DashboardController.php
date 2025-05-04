827976b9802e22a6200a1ad4bacb29726f20850e
ibraum <ibraumodnok@gmail.com>
2025-05-04 00:24:19 +0000

    admin dashboard

--- app/Http/Controllers/Auth/Admin/DashboardController.php
+++ app/Http/Controllers/Auth/Admin/DashboardController.php
@@ -3,7 +3,9 @@ namespace App\Http\Controllers\Auth\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Boutique;
+use App\Models\Categorie;
 use App\Models\Commande;
+use App\Models\Panier;
 use App\Models\Produit;
 use App\Models\User;
 
@@ -13,30 +15,43 @@ class DashboardController extends Controller
     {
         $stats= [];
         $boutiques = Boutique::with('produits.commandeItems')->get();
-        $commandes = Commande::all()->groupBy('status');
+        $commandes = Commande::all();
+        $commandesParStatus = Commande::all()->groupBy('status');
         $produits = Produit::withCount('commandeItems')
         ->orderBy('items_count', 'desc')
         ->take(5)
         ->get();
 
         $clients = User::withCount('commandes')
+        ->where('role', 'buyer')
         ->orderBy('commandes_count', 'desc')
         ->take(5)
         ->get();
 
+        $categories = Categorie::all();
+        $paniers = Panier::all();
+
         $ca = 0;
-        foreach ($commandes['paid'] as $value) {
+        foreach ($commandesParStatus['paid'] as $value) {
             $ca += $value->total;
         }
 
         $ca = 0;
-        foreach ($commandes['paid'] as $value) {
+        foreach ($commandesParStatus['paid'] as $value) {
             $ca += $value->total;
         }
         $revenusEspere = $ca;
-        foreach ($commandes['cancelled'] as $value) {
+        foreach ($commandesParStatus['cancelled'] as $value) {
             $revenusEspere += $value->total;
         }
-        return view('back.admin.stats', compact(['boutiques', 'commandes', 'produits', 'clients', 'ca', 'revenusEspere']));
+
+        $stats['boutiques'] = [$boutiques, 'bg-blue-500/10', 'fi fi-rr-store-alt', 'text-blue-500'];
+        $stats['commandes'] = [$commandes, 'bg-green-500/10', 'fi fi-rr-order-history', 'text-green-500'];
+        $stats['produits'] = [$produits, 'bg-red-500/10', 'fi fi-rr-box-open-full', 'text-red-500'];
+        $stats['clients'] = [$clients, 'bg-amber-500/10', 'fi fi-rr-membership', 'text-amber-500'];
+        $stats['categories'] = [$categories, 'bg-purple-500/10', 'fi fi-rr-category-alt', 'text-purple-500'];
+        $stats['paniers'] = [$paniers, 'bg-orange-500/10', 'fi fi-rr-cart-arrow-down', 'text-orange-500'];
+
+        return view('back.admin.stats', compact(['stats', 'commandesParStatus', 'ca', 'revenusEspere']));
     }
 }
--- resources/views/back/admin/stats.blade.php
+++ resources/views/back/admin/stats.blade.php
@@ -1,9 +1,18 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <div class="flex gap-3 border">
-        <div class="w-[300px] border">1</div>
-        <div class="w-[calc(100%-300px)] border">2</div>
+    @php
+        $as = [1, 2, 3, 4, 5]
+    @endphp
+    {{-- {{ dd($stats) }} --}}
+    <div class="flex gap-3">
+        <div class="w-[calc(100%-250px)] border rounded shadow px-2">2</div>
+        <div class="w-[250px] border flex p-2 rounded flex flex-wrap gap-3 shadow">
+            @foreach ($stats as $key => $stat)
+                <div class="{{ $stat[1] }} w-full h-[100px] border grow rounded flex items-center justify-start gap-3 pl-3">
+                    <div class="text-lg h-[50px] w-[50px] rounded-full flex items-center justify-center {{ $stat[3] }} {{ $stat[1] }}"><i class="{{ $stat[2] }}"></i></div> {{ count($stat[0]) }} - {{ $key }}
+                </div>
+            @endforeach
+        </div>
     </div>
-    {{ $products }}
 @endsection
